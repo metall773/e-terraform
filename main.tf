3d65105bed4df0b0b9cc6e8e3cf29e0746705a90
metall773 <64757081+metall773@users.noreply.github.com>
2020-07-04 22:05:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+variable "ARM_CLIENT_ID" {
+}
+variable "ARM_SUBSCRIPTION_ID" {
+}
+variable "ARM_CLIENT_SECRET" {
+}
+variable "ARM_TENANT_ID" {
+}
+
 terraform {
   backend "azurerm" {
     resource_group_name   = "terraform-state"
