74f4c07e2e773cb4e68a725573fed691610c56f4
metall773 <64757081+metall773@users.noreply.github.com>
2020-07-04 17:30:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,12 @@ provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x. 
   # If you're using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
+  
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT
+  
   features {}
 }
 
