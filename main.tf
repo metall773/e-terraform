ee80ad0917524bf1a1baaf9d4b9930263a844d33
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-05 11:47:06 +0200

    main.tf cleanup

--- main.tf
+++ main.tf
@@ -1,35 +1,3 @@
-variable "ARM_CLIENT_ID" {
-}
-variable "ARM_SUBSCRIPTION_ID" {
-}
-variable "ARM_CLIENT_SECRET" {
-}
-variable "ARM_TENANT_ID" {
-}
-
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "terraform-state"
-    storage_account_name  = "4terraform2state"
-    container_name        = "tstate"
-    key                   = "terraform.tfstate"
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x. 
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
-  
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
-  
-  features {}
-}
-
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "myterraformgroup" {
   name     = "myResourceGroup"
