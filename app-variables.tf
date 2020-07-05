850730acbe12408facc6a8e18dfc74d292f439a1
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-05 11:28:29 +0200

    slpit main.tf

---
+++ app-variables.tf
@@ -0,0 +1,19 @@
+# company name 
+variable "company" {
+    type        = string
+    description = "The company name used to build resources"
+    default     = "e-tiketka"
+}# application name 
+variable "app_name" {
+    type        = string
+    description = "The application name used to build resources"
+}# environment
+variable "environment" {
+    type        = string
+    description = "The environment to be built"
+}# azure region
+variable "location" {
+    type        = string
+    description = "Azure region where the resources will be created"
+    default     = "north europe"
+}
---
+++ provider-main.tf
@@ -0,0 +1,23 @@
+# State file location
+terraform {
+    backend "azurerm" {
+        resource_group_name   = "terraform-state"
+        storage_account_name  = "4terraform2state"
+        container_name        = "tstate"
+        key                   = "terraform.tfstate"
+    }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    
+    subscription_id = var.ARM_SUBSCRIPTION_ID
+    client_id       = var.ARM_CLIENT_ID
+    client_secret   = var.ARM_CLIENT_SECRET
+    tenant_id       = var.ARM_TENANT_ID
+    
+    features {}
+}
