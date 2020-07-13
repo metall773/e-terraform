f85c70db66cf546ab147b1cfcac3878456fd551e
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-13 22:34:47 +0200

    network fixes

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "azurerm_network_interface" "myterraformnic" {
 
   ip_configuration {
     name                          = "myNicConfiguration"
-    subnet_id                     = azurerm_subnet.myterraformsubnet.id
+    subnet_id                     = azurerm_subnet.network-vnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
   }
