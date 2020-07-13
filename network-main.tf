cb2273639119d23db2cf03e890120a6276d00d44
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-13 23:12:49 +0200

    stable ver

--- network-main.tf
+++ network-main.tf
@@ -26,8 +26,4 @@ resource "azurerm_subnet" "network-subnet" {
     address_prefixes     = var.network-subnet-cidr
     virtual_network_name = azurerm_virtual_network.network-vnet.name
     resource_group_name  = azurerm_resource_group.network-rg.name
-    tags = {
-        application = var.app_name
-        environment = var.environment
-    }
 }
