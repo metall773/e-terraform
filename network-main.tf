7b1a7969455da8c7e86a24b4c629af6cf916cc1a
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-13 23:24:40 +0200

    fix

--- network-main.tf
+++ network-main.tf
@@ -1,6 +1,6 @@
 # Create a resource group for network
 resource "azurerm_resource_group" "network-rg" {
-    name     = "${var.environment}-${var.app_name}-network=rg"
+    name     = "${var.environment}-${var.app_name}-network-rg"
     location = var.location
     tags = {
         application = var.app_name
