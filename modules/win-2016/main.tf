5fcf7d2ed9a989cdf1c1bca3702b15b74f04865a
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-21 00:49:39 +0200

    win server

--- modules/win-2016/main.tf
+++ modules/win-2016/main.tf
@@ -1,21 +1,36 @@
-resource "azurerm_network_interface" "example" {
+locals {
+  vm_name = "${var.environment}-${var.win-vm-prefix}-vm"
+}
+
+resource "azurerm_resource_group" "win-terraform-group" {
+  name     = local.vm_name
+  location = var.location
+
+  tags = {
+        application = var.app_name
+        environment = var.environment
+        vm-name     = local.vm_name
+  }
+}
+
+resource "azurerm_network_interface" "win_network_interface" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.win-terraform-group.location
+  resource_group_name = azurerm_resource_group.win-terraform-group.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = var.network-subnet
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
+resource "azurerm_windows_virtual_machine" "win_virtual_machine {
   name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.win-terraform-group.name
+  location            = azurerm_resource_group.win-terraform-group.location
   size                = "Standard_F2"
-  admin_username      = "adminuser"
+  admin_username      = var.win-admin-username
   admin_password      = "P@$$w0rd1234!"
   network_interface_ids = [
     azurerm_network_interface.example.id,
@@ -23,7 +38,7 @@ resource "azurerm_windows_virtual_machine" "example" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = var.storage_account_type
   }
 
   source_image_reference {
---
+++ modules/win-2016/variables.tf
@@ -0,0 +1,119 @@
+# environment
+variable "environment" {
+    type        = string
+    description = "The environment to be built"
+    default     = "dev"
+}
+
+# azure vm region
+variable "location" {
+    type        = string
+    description = "Azure region where the resources will be created"
+    default     = "north europe" # "germanywestcentral"
+}
+
+# application name 
+variable "app_name" {
+    type        = string
+}
+
+# Azure virtual machine settings #
+variable "win-vm-size" {
+    type        = string
+    description = "Size (SKU) of the virtual machine to create"
+    default     = "Standard_F2"
+}
+
+variable "win-license-type" {
+    type        = string
+    description = "Specifies the BYOL type for the virtual machine."
+    default     = null
+}
+
+# Azure virtual machine storage settings #
+variable "win-delete-os-disk-on-termination" {
+    type        = string
+    description = "Should the OS Disk (either the Managed Disk / VHD Blob) be deleted when the Virtual Machine is destroyed?"
+    default     = "true"  # Update for your environment
+}
+
+variable "win-delete-data-disks-on-termination" {
+    description = "Should the Data Disks (either the Managed Disks / VHD Blobs) be deleted when the Virtual Machine is destroyed?"
+    type        = string
+    default     = "true" # Update for your environment
+}
+
+variable "win-vm-image" {
+    type        = map(string)
+    description = "Virtual machine source image information"
+    default     = {
+        publisher = "OpenLogic"
+        offer     = "CentOS"
+        sku       = "7_8"
+        version   = "latest"
+    }
+}
+
+# Azure virtual machine OS profile #
+variable "win-admin-username" {
+    type        = string
+    description = "Username for Virtual Machine administrator account"
+    default     = "azureuser"
+}
+
+variable "storage_account_type" {
+    type        = string
+    description = "The storage account type for the Managed Disk"
+    default     = "Standard_LRS"
+}
+
+variable "managed_disk_size_gb" {
+    type        = string
+    description = "The size of the Managed Disk in gigabytes"
+    default     = "5"
+}
+
+variable "managed_disk_mount_point" {
+    type        = string
+    description = "path for mount managed disk"
+    default     = "m:"
+}
+
+variable "win-vm-prefix" {
+    default = "example"
+}
+
+variable "azurerm_storage_account_tier" {
+    type        = string
+    description = "Defines the Tier to use for this storage account."
+    default     = "Standard"
+}
+
+#https://docs.microsoft.com/en-us/azure/storage/common/storage-redundancy#redundancy-in-the-primary-region
+variable "azurerm_account_replication_type" {
+    type        = string
+    description = "Data in an Azure Storage account is always replicated three times in the primary region."
+    default     = "LRS"
+}
+
+variable "azurerm_storage_share_quota" {
+    type        = string
+    description = "The maximum size of the share, in gigabytes."
+    default     = "50"
+}
+
+variable "network-subnet" {
+    type        = string
+}
+
+variable "firewall_tcp_ports" {
+    type        = list
+    description = "list ports to allow incoming tcp connections by cloud-init"
+    default     = [3389]
+}
+
+variable "firewall_udp_ports" {
+    type        = list
+    description = "list ports to allow incoming udp connections by cloud-init"
+    default     = [3389]
+}
