f194ee715764c8b8efcbb724eff4d31747eb0266
Johnny Lee <lee040404@gmail.com>
2020-07-26 08:41:53 +0200

    change mysql name

--- modules/mysql/output.tf
+++ modules/mysql/output.tf
@@ -1,7 +1,7 @@
 output "fqdn" {
-    value = azurerm_mysql_server.mysql-db.fqdn
+    value = azurerm_mysql_server.mysql-server.fqdn
 }
 
 output "username" {
-    value = "${azurerm_mysql_server.mysql-db.administrator_login}@${azurerm_mysql_server.mysql-db.name}"
+    value = "${azurerm_mysql_server.mysql-server.administrator_login}@${azurerm_mysql_server.mysql-server.name}"
 }
