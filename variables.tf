2a97c5cb23ed17a63a6c1df11361533cea74d84c
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-28 00:10:40 +0200

    change repo style

--- variables.tf
+++ variables.tf
@@ -25,3 +25,24 @@ variable "location" {
     description = "Azure region where the resources will be created"
     default     = "north europe" # "germanywestcentral"
 }
+
+#Azure authentication variables
+variable "ARM_SUBSCRIPTION_ID" {
+  type = string
+  description = "Azure Subscription ID"
+}
+
+variable "ARM_CLIENT_ID" {
+  type = string
+  description = "Azure Client ID"
+}
+
+variable "ARM_CLIENT_SECRET" {
+  type = string
+  description = "Azure Client Secret"
+}
+
+variable "ARM_TENANT_ID" {
+  type = string
+  description = "Azure Tenant ID"
+}
