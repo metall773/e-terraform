08d548eff07be17f8b48bcfb7ca05b3d9c050e56
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-28 10:29:27 +0200

    variables update

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,9 @@
+#envirment variables
+company     = "e-tiketka"
+app_name    = "e-tiketka-app"
+environment = "az-dev"
+location    = "north europe"
+
 #vm network variables
 network-vnet-cidr   = "10.16.0.0/22"   #10.0.0.0 - 10.0.3.255
 network-subnet-cidr = ["10.16.1.0/24"]
--- variables.tf
+++ variables.tf
@@ -2,28 +2,24 @@
 variable "company" {
     type        = string
     description = "The company name used to build resources"
-    default     = "e-tiketka"
 }
 
 # application name 
 variable "app_name" {
     type        = string
     description = "The application name used to build resources"
-    default     = "e-tiketka-app"
 }
 
 # environment
 variable "environment" {
     type        = string
     description = "The environment to be built"
-    default     = "dev"
 }
 
 # azure vm region
 variable "location" {
     type        = string
     description = "Azure region where the resources will be created"
-    default     = "north europe" # "germanywestcentral"
 }
 
 #Azure authentication variables
