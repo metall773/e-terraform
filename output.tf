54b63febef1972b5bf286b8ecd2b45f1c2a94228
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-29 23:38:43 +0200

    update output

--- output.tf
+++ output.tf
@@ -1,21 +1,39 @@
-output "centos7_vm" {
-    value = "ssh ${module.centos7-vm.username}@${module.centos7-vm.fqdn}"
+output "centos7_vm1" {
+    value = "ssh ${module.centos7-vm1.username}@${module.centos7-vm1.fqdn}"
 }
 
 output "centos7_vm2" {
     value = "ssh ${module.centos7-vm2.username}@${module.centos7-vm2.fqdn}"
 }
 
-output "win1-vm" {
-    value = "rdp ${module.win1-vm.username}@${module.win1-vm.fqdn}"
+output "centos7_vm3" {
+    value = "ssh ${module.centos7-vm3.username}@${module.centos7-vm3.fqdn}"
 }
 
-output "centos7_vm_db" {
+output "centos7_vm4" {
+    value = "ssh ${module.centos7-vm4.username}@${module.centos7-vm4.fqdn}"
+}
+
+output "centos7_vm5" {
+    value = "ssh ${module.centos7-vm5.username}@${module.centos7-vm5.fqdn}"
+}
+
+output "centos7_vm6" {
+    value = "ssh ${module.centos7-vm6.username}@${module.centos7-vm6.fqdn}"
+}
+
+output "centos7_vm7" {
+    value = "ssh ${module.centos7-vm7.username}@${module.centos7-vm7.fqdn}"
+}
+
+output "win-vm1" {
+    value = "rdp ${module.win1-vm.username}@${module.win-vm1.fqdn}"
+}
+
+output "win-vm2" {
+    value = "rdp ${module.win1-vm.username}@${module.win-vm2.fqdn}"
+}
+
+output "win-vm2-db" {
     value = "pqsl -U ${module.postgresql_db1.username} -h ${module.postgresql_db1.fqdn} -d test"
 }
-
-output "centos7_vm2_db" {
-    value = "mysql -h ${module.mysql_db1.fqdn} -u ${module.mysql_db1.username} -p"
-}
-
-
