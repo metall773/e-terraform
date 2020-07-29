1f6bb50d102c95c6b79a093d1d7cd4ae6abf3b88
Evgeniy Sevostyanov <Evgeniy.Sevostyanov@tipico.com>
2020-07-29 23:20:56 +0200

    add missed variables

--- variables.tf
+++ variables.tf
@@ -46,3 +46,15 @@ variable "ARM_TENANT_ID" {
   type = string
   description = "Azure Tenant ID"
 }
+
+variable "network-vnet-cidr" {
+    type        = string
+    description = "The CIDR of the network VNET"
+    default     = "10.16.0.0/22"
+}
+
+variable "network-subnet-cidr" {
+    type        = list
+    description = "The CIDR for the network subnet"
+    default     = ["10.16.1.0/24"]
+}
